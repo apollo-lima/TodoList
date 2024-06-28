eb51d4d057cfd6f4960b80ba16392911c5afc430
peoux <apollo.lima@icloud.com>
2024-06-28 23:11:10 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Apollo Lima on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
